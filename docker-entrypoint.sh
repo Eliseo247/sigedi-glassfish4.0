32ebb3a5320aa12568b890a86cb956ea7e529235
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-08-17 13:59:40 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 if [ "$1" = 'asadmin' ]; then
     if [ "$AS_ADMIN_PASSWORD" ]; then
         echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
         echo "AS_ADMIN_NEWPASSWORD=${AS_ADMIN_PASSWORD}" >> /tmp/glassfishpwd
 
         # Change the admin password
-        asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
+        asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1 && id
+
+       su - exemple
 
         # Log in the admin user
         # 
@@ -19,14 +27,17 @@ if [ "$1" = 'asadmin' ]; then
         # Therefore, manually generate the .asadminpass file to "login".
         
         AS_ADMIN_PASSWORD_GFBASE64=`echo -n "$AS_ADMIN_PASSWORD" | base64`
-        echo "asadmin://admin@localhost:4848 $AS_ADMIN_PASSWORD_GFBASE64" > /root/.asadminpass
-        chmod 600 /root/.asadminpass
+        echo "asadmin://admin@localhost:4848 $AS_ADMIN_PASSWORD_GFBASE64" > /root/.asadminpass && id
+su - exemple
+        chmod 600 /root/.asadminpass && id
+su - exemple
 
         if [ "$AS_ADMIN_ENABLE_SECURE" ]; then
             echo "AS_ADMIN_PASSWORD=${AS_ADMIN_PASSWORD}" > /tmp/glassfishpwd
             asadmin start-domain            
             
-            asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+            asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin && id
+su - exemple
             
             # Call asadmin at least once to establish a trust with the
             # self-signed certificate for the admin API.
@@ -42,4 +53,3 @@ if [ "$1" = 'asadmin' ]; then
 fi
 
 exec "$@"
-asadmin --host localhost --port 4848 enable-secure-admin
