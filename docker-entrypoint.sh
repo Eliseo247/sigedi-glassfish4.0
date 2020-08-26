70d3ffab76fc47238b1707801bbddec26181527c
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-08-26 12:54:56 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 #Copiar los archivos desde el backup si no existen en la ruta /opt/glassfish4/, previamente se hizo un backup cp -Rf * /home/digitalizados/backupbinarios-latest
 
 #if [ ! -f /opt/glassfish4/bin/asadmin.bat]; then
-  cp -Rf /home/digitalizados/backupbinarios-latest/* /opt/glassfish4 
+  #cp -Rf /home/digitalizados/backupbinarios-latest/* /opt/glassfish4 
  # fi
   
 
