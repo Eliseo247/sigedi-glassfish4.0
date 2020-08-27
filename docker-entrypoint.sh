6299152ac2c402a75aa18602ab08d2d2386c0c77
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-08-27 15:29:38 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,16 +7,4 @@
  # fi
   
 
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
 
-
-           asadmin start-domain            
-           
-   
-
-
-exec "$@"
