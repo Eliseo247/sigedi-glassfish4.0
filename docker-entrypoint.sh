cc2e4e7605ddc21bd3e9ff69ea4e22176e093ae2
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-08-28 07:55:16 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,6 @@
 #############################################################################################################
 #Copiar los archivos desde el backup si no existen en la ruta /opt/glassfish4/, previamente se hizo un backup cp -Rf * /home/digitalizados/backupbinarios-latest
 
-#if [ ! -f /opt/glassfish4/bin/asadmin.bat]; then
- cp -Rf /home/digitalizados/backupbinarios-latest/* /opt/glassfish4/ 
- # fi
-  
-  #!/bin/sh
 
 if [ "$1" = 'asadmin' ]; then
     if [ "$AS_ADMIN_PASSWORD" ]; then
@@ -42,7 +37,9 @@ if [ "$1" = 'asadmin' ]; then
 
             asadmin stop-domain
         fi
-
+#if [ ! -f /opt/glassfish4/bin/asadmin.bat]; then
+ cp -Rf /home/digitalizados/backupbinarios-latest/* /opt/glassfish4/ 
+ # fi
         rm /tmp/glassfishpwd
     fi
 
